412d54d0a24699211b721587900ee48fac64e53b
Malique-Auguste <maliqueauguste@gmail.com>
2020-12-12 19:25:50 +0000

    Fixed motor code
    Since the motor had the line instead of linehandle, when the function using the linehandle ended, the linehadle was destroyed and thus whatever changes were made to that p[in were also 'destroyed'
    This was solved by making the motor contain the linehandle instead of line so that the changes made to a pin are only destroyed/reverted when the motor goes out of scope.

--- src/main.rs
+++ src/main.rs
@@ -18,19 +18,6 @@ fn main() {
     println!("\n\nHello, world!");
     let mut chip = Chip::new("/dev/gpiochip0").unwrap();
     
-    /*
-    let line = chip.get_line(24).unwrap().request(LineRequestFlags::OUTPUT, 1, "motor 1.1").unwrap();
-    let line2 = chip.get_line(25).unwrap().request(LineRequestFlags::OUTPUT, 1, "motor 1.1").unwrap();
-    println!("{:?}", line);
-    println!("{:?}", line2);
-    println!("{:?}", line.set_value(1));
-    println!("{:?}", line2.set_value(1));
-    
-    let t = time::Instant::now();
-    while t.elapsed().as_secs_f32() < 3.5 {}
-    */
-    
-    
     let mut motor = DCMotor::new(&mut chip, (25, 23, 24), None).unwrap();
     println!("\nMotor {:?}", motor);
 
